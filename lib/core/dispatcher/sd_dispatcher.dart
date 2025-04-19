64d7c3d8b6323491af4fe7162a3b8030e4ff4731
Dwiki Riyadi <wiki.riyadi@gmail.com>
2025-04-20 06:58:26 +0700

    init dispatcher & registry files

--- lib/core/dispatcher/sd_dispatcher.dart
+++ lib/core/dispatcher/sd_dispatcher.dart
@@ -3,22 +3,20 @@ import 'package:json_ui/core/widget/sd_item_widget.dart';
 typedef SDRender = SDItemWidget Function(Map<String, dynamic>);
 
 class SDDispatcher {
-  static Map<String, SDRender> renderers = {};
+  static final Map<String, SDRender> _renderers = {};
 
   static void register({
     required String componentName,
     required SDRender widget,
   }) {
-    renderers[componentName] = widget;
+    _renderers[componentName] = widget;
   }
 
-  static SDItemWidget? render({
-    required Map<String, dynamic> item,
-  }) {
-    return renderers[item["widget"]]?.call(item);
+  static SDItemWidget? render({required Map<String, dynamic> item}) {
+    return _renderers[item["widget"]]?.call(item);
   }
 
   static void clear() {
-    renderers.clear();
+    _renderers.clear();
   }
 }
---
+++ lib/core/dispatcher/validator_dispatcher.dart
@@ -0,0 +1,22 @@
+import 'package:json_ui/core/validation/validator.dart';
+
+typedef ValidatorFactory = Validator Function(Map<String, dynamic>);
+
+class ValidatorDispatcher {
+  static final Map<String, ValidatorFactory> _registered = {};
+
+  static void register({
+    required String name,
+    required ValidatorFactory factory,
+  }) {
+    _registered[name] = factory;
+  }
+
+  static Validator? resolve(Map<String, dynamic> data) {
+    return _registered[data["name"]]?.call(data);
+  }
+
+  static void clear() {
+    _registered.clear();
+  }
+}
